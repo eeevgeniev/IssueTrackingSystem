68c74041ca9ad7d793b9570be575de28d9228308
Emiliyan Evgeniev <mr_e@abv.bg>
2016-04-14 17:41:24 +0300

    New Changes

--- scripts/trackingManagerApp/controllers/users/user.js
+++ scripts/trackingManagerApp/controllers/users/user.js
@@ -1,17 +1,43 @@
 (function () {
-    var userModule = angular.module('TrackingManagerApp.Controllers.Users.User', []);
+    var userModule = angular.module('TrackingManagerApp.Controllers.Users.User', ['TrackingManagerApp.Https.Request']);
 
-    userModule.controller('UserController', ['$scope', function ($scope) {
+    userModule.controller('UserController', ['$scope', '$q', 'Requests', function ($scope, $q, Requests) {
 
-        $scope.title = 'Start page';
         $scope.heading = 'Welcome guest. Login or register.';
 
         $scope.login = function login() {
-            console.log(JSON.stringify($scope.user));
+            var promise = Requests.login($scope.user);
+
+            promise.then(function success(response) {
+                var userCredentials = {};
+                userCredentials.token = response.data.access_token;
+                userCredentials.name = response.data.userName;
+                console.log(JSON.stringify(userCredentials));
+            }, function error(response) {
+                console.log(response.data.ModelState[''][1]);
+            })
         }
 
         $scope.register = function register() {
-            console.log(JSON.stringify($scope.user));
+            var promise = Requests.send($scope.newUser);
+
+            promise.then(function success(response) {
+                var user = {};
+
+                user.email = $scope.newUser.email;
+                user.password = $scope.newUser.password;
+
+                Requests.login(user);
+            }, function error(response) {
+                for (var property in response.data.ModelState) {
+                    if (property === 'model.Email' || property === 'model.Password' || property === '' || property === 'model.ConfirmPassword') {
+                        console.log(response.data.ModelState[property][0]);
+                        return;
+                    }
+                }
+
+                console.log('Error registration');
+            })
         };
     }]);
 })();
