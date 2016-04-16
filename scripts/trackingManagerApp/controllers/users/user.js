17eb133319594b3c8d2a5b16e17c5f6dd0857392
Emiliyan Evgeniev <mr_e@abv.bg>
2016-04-16 10:46:09 +0300

    Revert "New Changes"
    This reverts commit 68c74041ca9ad7d793b9570be575de28d9228308.

--- scripts/trackingManagerApp/controllers/users/user.js
+++ scripts/trackingManagerApp/controllers/users/user.js
@@ -1,43 +1,17 @@
 (function () {
-    var userModule = angular.module('TrackingManagerApp.Controllers.Users.User', ['TrackingManagerApp.Https.Request']);
+    var userModule = angular.module('TrackingManagerApp.Controllers.Users.User', []);
 
-    userModule.controller('UserController', ['$scope', '$q', 'Requests', function ($scope, $q, Requests) {
+    userModule.controller('UserController', ['$scope', function ($scope) {
 
+        $scope.title = 'Start page';
         $scope.heading = 'Welcome guest. Login or register.';
 
         $scope.login = function login() {
-            var promise = Requests.login($scope.user);
-
-            promise.then(function success(response) {
-                var userCredentials = {};
-                userCredentials.token = response.data.access_token;
-                userCredentials.name = response.data.userName;
-                console.log(JSON.stringify(userCredentials));
-            }, function error(response) {
-                console.log(response.data.ModelState[''][1]);
-            })
+            console.log(JSON.stringify($scope.user));
         }
 
         $scope.register = function register() {
-            var promise = Requests.send($scope.newUser);
-
-            promise.then(function success(response) {
-                var user = {};
-
-                user.email = $scope.newUser.email;
-                user.password = $scope.newUser.password;
-
-                Requests.login(user);
-            }, function error(response) {
-                for (var property in response.data.ModelState) {
-                    if (property === 'model.Email' || property === 'model.Password' || property === '' || property === 'model.ConfirmPassword') {
-                        console.log(response.data.ModelState[property][0]);
-                        return;
-                    }
-                }
-
-                console.log('Error registration');
-            })
+            console.log(JSON.stringify($scope.user));
         };
     }]);
 })();
--- scripts/trackingManagerApp/https/requests.js
+++ scripts/trackingManagerApp/https/requests.js
@@ -1,44 +1,24 @@
 ﻿(function () {
-    var httpRequest = angular.module('TrackingManagerApp.Https.Request', ['TrackingManagerApp.Https.Helper']);
+    var httpRequest = angular.module('TrackingManagerApp.Https.Request', []);
 
-    httpRequest.factory('Requests', ['$http', '$q', 'Urls', 'EncodeURIComponent', function ($http, $q, Urls, EncodeURIComponent) {
-        var requests = {};
-
-        requests.login = function login(user) {
-            var data = 'grant_type=password&username=' +
-                EncodeURIComponent.encode(user.email) + '&password=' + EncodeURIComponent.encode(user.password);
-
-            var promise = httpRequest(Urls.DefaultUrl + Urls.Login, { 'Content-Type': 'application/x-www-form-urlencoded' }, 'POST', data);
-
-            return promise;
-        }
-
-        requests.register = function register(user) {
-            var promise = httpRequest(Urls.DefaultUrl + Urls.Register, null, 'POST', user);
-
-            return promise;
-        }
-
-        function httpRequest(location, headersData, methodType, bodyData) {
-            headersData = headersData || { 'Content-type': 'application/json' };
-
-            var deferred = $q.defer();
+    httpRequest.factory('Requests', ['', function () {
+        function request(url, method, success, error, data, headers) {
+            headers = headers || { 'Content-type': 'application/json' };
 
             $http({
-                url: location,
-                headers: headersData,
-                method: methodType,
-                data: bodyData
-            })
-                .then(function (response) {
-                    deferred.resolve(response);
-                }, function (response) {
-                    deferred.reject(response);
-                });
-
-            return deferred.promise;
+                'url': url,
+                'headers': headers,
+                'method': method,
+                'data': data,
+            }).then(function (response) {
+                success(response);
+            }, function (response) {
+                error(response);
+            });
         }
 
-        return requests;
+        return {
+            request: request(url, method, success, error, data, headers)
+        }
     }]);
 })();
