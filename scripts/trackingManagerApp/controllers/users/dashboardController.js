92921530304ad8c5a6a6bf3e1416fc590cd116bd
Emiliyan Evgeniev <mr_e@abv.bg>
2016-04-29 10:45:05 +0300

    Added modal service

--- scripts/trackingManagerApp/controllers/users/dashboardController.js
+++ scripts/trackingManagerApp/controllers/users/dashboardController.js
@@ -1,16 +1,28 @@
 ﻿(function () {
-    var dashboardModule = angular.module('trackingManagerApp.controllers.users.dashboardController', 
-    ['trackingManagerApp.services.commands.userServices']);
+    var dashboardModule = angular.module('trackingManagerApp.controllers.users.dashboardController',
+    ['trackingManagerApp.services.commands.userServices', 'trackingManagerApp.services.commands.modalServices']);
 
-    dashboardModule.controller('DashboardController', ['$scope', '$q', 'userServices', function ($scope, $q, userServices) {
+    dashboardModule.controller('DashboardController', ['$scope', '$q', 'userServices', 'modalServices',
+        function ($scope, $q, userServices, modalServices) {
 
-        var promise = userServices.getUserIssues(10, 1, 'Title');
+            var promise = userServices.getUserIssues(10, 1, 'Title');
 
-        promise.then(function success(response) {
-            $scope.issues = response.data.Issues;
-        }, function error(response) {
-            console.log(response);
-        });
+            promise.then(function success(response) {
+                $scope.issues = response.data.Issues;
+            }, function error(response) {
+                console.log(response);
+            });
 
-    }]);
+            $scope.newProject = function newProject() {
+                modalServices.createModal('#new-project', 700, 800);
+            }
+
+            $scope.newIssue = function newIssue() {
+                modalServices.createModal('#new-issue', 700, 800);
+            }
+
+            $scope.$on('$destroy', function () {
+                $('.ui-dialog').remove();
+            });
+        }]);
 })();
--- scripts/trackingManagerApp/services/commands/modalServices.js
+++ scripts/trackingManagerApp/services/commands/modalServices.js
@@ -4,6 +4,24 @@
     modalCommands.factory('modalServices', [function () {
         var commands = {};
 
+        commands.createModal = function createModal(elementId, modalHeight, modalWidth) {
+            var projectDialog = $(elementId).dialog({
+                autoOpen: false,
+                height: modalHeight,
+                width: modalWidth,
+                modal: true,
+                buttons: {
+                    Cancel: function () {
+                        projectDialog.dialog("close");
+                    }
+                }, Close: function () {
+                    form[0].reset();
+                }
+            });
+
+            $(elementId).removeClass('hidden');
+            projectDialog.dialog('open');
+        }
 
         return commands;
     }]);
